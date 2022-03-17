4c2227a0e4731f765b7193a1e0d6a14eb51b9ae1
Sourav Das <sdunity12061999@gmail.com>
2022-03-17 10:57:14 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    masai
-    </div>
-  );
+  return <div className="App">Sourav</div>;
 }
 
 export default App;
